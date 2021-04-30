a1fd7c1e5a5e1c8a66871b54d9396e522cca5273
Victor Orlunda <vicorlunda@gmail.com>
2021-05-01 00:36:13 +0100

    refactor: remove async error handlers from users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,86 +1,73 @@
 const User = require('../models/Users');
-const serverErrors = require('../utils/errorHandler');
 
 const getUsers = async (req, res) => {
-  try {
-    if (req.query.pageSize) {
-      const { pageSize } = req.query;
+  if (req.query.pageSize) {
+    const { pageSize } = req.query;
 
-      const users = await User.find({})
-        .select('-__v -login_details')
-        .limit(+pageSize)
-        .lean();
+    const users = await User.find({})
+      .select('-__v -login_details')
+      .limit(+pageSize)
+      .lean();
 
-      res.json({
-        message: 'Welcome to the users endpoint',
-        users,
-      });
-    } else {
-      const users = await User.find({})
-        .select('-__v -login_details')
-        .limit(10)
-        .lean();
+    res.json({
+      message: 'Welcome to the users endpoint',
+      users,
+    });
+  } else {
+    const users = await User.find({})
+      .select('-__v -login_details')
+      .limit(10)
+      .lean();
 
-      res.json({
-        message: 'Welcome to the users endpoint',
-        users,
-      });
-    }
-  } catch (error) {
-    serverErrors(res, error);
+    res.json({
+      message: 'Welcome to the users endpoint',
+      users,
+    });
   }
 };
 
 const getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
+  const user = await User.findById(req.params.id)
+    .select('-__v -login_details')
+    .lean();
+
+  if (!user) {
+    return res.status(404).json({
+      notice: 'nothing found',
+      reason: `no user with the id of '${req.params.id}' in the database`,
+    });
+  }
+
+  res.json({
+    message: 'user found',
+    user,
+  });
+};
+
+const updateUser = async (req, res) => {
+  const { username } = req.body;
+  const { id } = req.params;
+
+  const result = await User.updateOne({ _id: id }, { username });
+
+  if (result.nModified < 1) {
+    const user = await User.findOne({ _id: id })
       .select('-__v -login_details')
       .lean();
 
     if (!user) {
-      return res.status(404).json({
-        notice: 'nothing found',
-        reason: `no user with the id of '${req.params.id}' in the database`,
+      return res.status(400).json({
+        notice: 'operation failed',
+        reason: `no user with id of '${id}' found`,
       });
     }
 
-    res.json({
-      message: 'user found',
-      user,
-    });
-  } catch (error) {
-    serverErrors(res, error);
+    return res.json(user);
   }
-};
 
-const updateUser = async (req, res) => {
-  try {
-    const { username } = req.body;
-    const { id } = req.params;
-
-    const result = await User.updateOne({ _id: id }, { username });
-
-    if (result.nModified < 1) {
-      const user = await User.findOne({ _id: id })
-        .select('-__v -login_details')
-        .lean();
-
-      if (!user) {
-        return res.status(400).json({
-          notice: 'operation failed',
-          reason: `no user with id of '${id}' found`,
-        });
-      }
-
-      return res.json(user);
-    }
-
-    res.json({
-      message: `user with '${id}' username updated`,
-    });
-  } catch (error) {
-    serverErrors(res, error);
-  }
+  res.json({
+    message: `user with '${id}' username updated`,
+  });
 };
 
 const deleteUser = async (req, res) => {
